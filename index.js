11924f87f2a1a775c08d00486a82f035eff95443
Uri Shaked <uri@salsa4fun.co.il>
2017-03-13 23:07:23 +0200

    Add sunset demo

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello world!');
+const fetch = require('node-fetch');
+
+async function sunsetTime(date) {
+    const resp = await fetch(`http://api.sunrise-sunset.org/json?lat=32.095&lng=34.847&formatted=0&date=${date}`)
+    const result = await resp.json();
+    return new Date(result.results.sunset);
+}
+
+async function *getSunsets() {
+    for (let day = 1; day <= 31; day++) {
+        yield await sunsetTime('2017-03-' + day);
+    }
+}
+
+async function main() {
+    console.log('Hello Dev.IL 😊 !\n\n');
+    for await (let sunset of getSunsets()) {
+        console.log('Sunset time for', sunset.toDateString(),
+            'is at', sunset.toTimeString());
+    }
+}
+
+main();
